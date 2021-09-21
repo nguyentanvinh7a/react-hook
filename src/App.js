c23dfe721259dd659a9322e365079cb1f1546fa1
Vinh Nguyen <nguyentanvinh2911@gmail.com>
2021-09-21 15:28:30 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="App-header">
+        <h1>Welcome to React Hooks!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
